0059ade1a11c1cb26bf3d835340a38afee344661
awamper <awamper@gmail.com>
2014-07-24 16:54:05 +0800

    ResultsView: move StatusBox to results_status_box.js

--- results_view.js
+++ results_view.js
@@ -4,121 +4,15 @@ const Signals = imports.signals;
 const Separator = imports.ui.separator;
 const Main = imports.ui.main;
 const Params = imports.misc.params;
-const Tweener = imports.ui.tweener;
 
 const Me = imports.misc.extensionUtils.getCurrentExtension();
 const Utils = Me.imports.utils;
 const PrefsKeys = Me.imports.prefs_keys;
+const ResultsStatusBox = Me.imports.results_status_box;
 
 const CONNECTION_IDS = {
     OVERVIEW_HIDING: 0
 };
-
-const StatusBox = new Lang.Class({
-    Name: 'GriloResultsView.StatusBox',
-
-    _init: function(params) {
-        this.params = Params.parse(params, {
-            min_scale: 0.7,
-            padding: 20,
-            box_style_class: '',
-            label_style_class: ''
-        });
-        this.actor = new St.BoxLayout({
-            vertical: false,
-            style_class: this.params.box_style_class
-        });
-        this.actor.hide();
-
-        this._message = new St.Label({
-            text: '...',
-            style_class: this.params.label_style_class
-        });
-
-        this.actor.add_child(this._message);
-
-        this._minimized = false;
-        this._shown = false;
-
-        Main.uiGroup.add_child(this.actor);
-    },
-
-    _set_position: function(x, y, animate) {
-        if(!animate) {
-            this.actor.translation_x = x;
-            this.actor.translation_y = y;
-            return;
-        }
-
-        Tweener.removeTweens(this.actor);
-        Tweener.addTween(this.actor, {
-            time: 0.5,
-            transition: 'easeOutQuad',
-            translation_x: x,
-            translation_y: y
-        });
-    },
-
-    _reposition: function() {
-        let monitor = Main.layoutManager.currentMonitor;
-        let x, y;
-
-        if(!this._minimized) {
-            x = Math.floor(monitor.width / 2 - this.actor.width / 2);
-            y = Math.floor(monitor.height / 2 - this.actor.height / 2);
-        }
-        else {
-            x = Math.floor(
-                monitor.width - this.actor.width * this.params.min_scale
-            );
-            y = Math.floor(
-                monitor.height - this.actor.height * this.params.min_scale
-            );
-        }
-
-        this._set_position(x, y, this._shown);
-    },
-
-    show: function() {
-        if(this._shown) return;
-
-        this._shown = true;
-        this._reposition();
-        this.actor.show();
-
-        Main.uiGroup.set_child_above_sibling(this.actor, null);
-    },
-
-    hide: function() {
-        if(!this._shown) return;
-
-        this._shown = false;
-        this.actor.hide();
-    },
-
-    minimize: function() {
-        if(this._minimized) return;
-        this.actor.set_scale(this.params.min_scale, this.params.min_scale);
-        this._minimized = true;
-        this._reposition();
-    },
-
-    maximize: function() {
-        if(!this._minimized) return;
-        this.actor.set_scale(1, 1);
-        this._minimized = false;
-        this._reposition();
-    },
-
-    destroy: function() {
-        this.actor.destroy();
-    },
-
-    set text(text) {
-        this._message.set_text(text);
-        this._reposition();
-    }
-});
 
 const ResultRow = new Lang.Class({
     Name: 'GriloResultsView.ResultRow',
@@ -240,7 +134,7 @@ const ResultsView = new Lang.Class({
         this.actor.add_actor(this._box);
 
         this._rows = [];
-        this._status_box = new StatusBox({
+        this._status_box = new ResultsStatusBox.ResultsStatusBox({
             box_style_class: 'grilo-status-box',
             label_style_class: 'grilo-status-box-text'
         });
